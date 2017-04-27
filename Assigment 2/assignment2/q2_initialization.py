2bb95cd0eb652c396c142de0176003d09692bd67
Twice22 <victor.busa@gmail.com>
2017-04-27 10:04:12 +0200

    transform matrix into tf variable in in q2_initialization

--- Assigment 2/assignment2/q2_initialization.py
+++ Assigment 2/assignment2/q2_initialization.py
@@ -25,8 +25,8 @@ def xavier_weight_init():
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
         ### YOUR CODE HERE
-        epsilon = tf.sqrt(6 / sum(shape))
-        out = tf.random_uniform(shape, -epsilon, epsilon)
+        epsilon = tf.sqrt(6. / sum(shape))
+        out = tf.Variable(tf.random_uniform(shape, -epsilon, epsilon), **kwargs)
         ### END YOUR CODE
         return out
     # Returns defined initializer function.
